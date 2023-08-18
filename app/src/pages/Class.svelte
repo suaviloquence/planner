a9b579f2c36c073e8c8fb6477ecff716be003215
m <m@mcarr.one>
2023-08-18 00:14:33 -0700

    all in a day's work

--- app/src/pages/Class.svelte
+++ app/src/pages/Class.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
-	import { Class } from "../models/class";
+	import { type Class } from "../models/class";
 	import { api } from "../stores";
 
 	export let department: string;
 	export let number: string;
-	let info: Class = null;
-	$: $api.getClass({ department, number }).then((cls) => (info = cls));
+
+	// TODO: kinda hacky
+	let dept: string;
+	let num: string;
+	$: dept = department.toUpperCase();
+	$: num = number.toUpperCase();
+
+	let infoPromise: Promise<Class>;
+	$: infoPromise = $api.getClass({ department: dept, number: num });
 </script>
 
-<h1><span class="code">{department} {number}</span> - {info.name}</h1>
-<p class="description">{info.description}</p>
+{#await infoPromise}
+	Loading {dept} {num}
+{:then info}
+	<h1>
+		<!-- use the canonical name when we have it. -->
+		<span class="code">{info.code.department} {info.code.number}</span> - {info.name}
+	</h1>
+
+	<p class="description">{info.description}</p>
+	<ul>
+		<li><span class="label">Credits: </span>{info.units}</li>
+		{#if info.geCode}
+			<li><span class="label">General education: </span>{info.geCode}</li>
+		{/if}
+	</ul>
+{/await}
 
 <style>
 	.code {
+		font-weight: bold;
+	}
+
+	.label {
 		font-weight: bold;
 	}
 </style>
